1967a94e70feb1f84d0273566406124114f15b38
Jacob <jacob.robinett@ou.edu>
2018-10-03 11:50:30 -0500

    fix 3 works.  this tests custom messages

--- app.py
+++ app.py
@@ -9,15 +9,23 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+KEY_WORDS	=['hi', 'hello', 'yo']
+
 @app.route('/', methods=['POST'])
 def webhook():
   data = request.get_json()
   log('Recieved {}'.format(data))
 
+  #helper function
+  used_any = lambda word_list: any(map(lambda x: x in text, words_list))
+
   # We don't want to reply to ourselves!
-  if data['name'] != 'demobot56':
-    #msg = '{}, you sent "{}".'.format(data['name'], data['text'])
-    msg = 'what'
+  #if data['name'] != 'demobot56':
+    ##msg = '{}, you sent "{}".'.format(data['name'], data['text'])
+    #msg = 'what'
+    #send_message(msg)
+	if used_any(KEY_WORDS):
+    msg = 'hello to you'
     send_message(msg)
 
   return "ok", 200
